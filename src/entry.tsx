1602f5721e5e810e3f7285c665f3e3a52b69d0cb
Cesar Lai <git@cesarlai.com>
2022-05-08 00:05:48 +0800

    feat: use React 18

--- src/entry.tsx
+++ src/entry.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
 import '@/styles/global.css'
 
-ReactDOM.render(<App />, document.getElementById('cesarlai-app'))
+createRoot(document.getElementById('cesarlai-app')!).render(<App />)
